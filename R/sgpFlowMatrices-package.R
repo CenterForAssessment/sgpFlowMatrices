f9950d0a6fad6edfa4433e1ddfc853ee43870040
Damian Betebenner <dbetebenner@nciea.org>
2025-05-12 16:19:08 -0400

    Updating matrices for sgp.quantiles 1 to 99

--- R/sgpFlowMatrices-package.R
+++ R/sgpFlowMatrices-package.R
@@ -14,8 +14,8 @@
 #' \tabular{ll}{
 #'   Package: \tab sgpFlow \cr
 #'   Type: \tab Package \cr
-#'   Version: \tab 0.0-0.989 \cr
-#'   Date: \tab 2025-5-5 \cr
+#'   Version: \tab 0.0-0.5 \cr
+#'   Date: \tab 2025-5-12 \cr
 #'   License: \tab MIT \cr
 #'   LazyLoad: \tab yes \cr
 #' }
